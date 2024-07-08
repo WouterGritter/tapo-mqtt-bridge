a47db0a896fd60cbedfadc4b3269954d23278955
Wouter Gritter <wouter@gritter.me>
2024-07-09 00:08:59 +0200

    Fix reauthentication issue

--- device_utils.py
+++ device_utils.py
@@ -10,6 +10,10 @@ def execute_device_method(device: T, function: Callable[[T], R]) -> R:
     try:
         return function(device)
     except:
+        # Force protocol to be None to re-authenticate. The library does not support this directly, and it can cause
+        # issues when the script is running for a long time.
+        device.protocol = None
+
         device.handshake()
         device.login()
 
